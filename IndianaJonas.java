77dc99047944b364b6d310ae30c1a7b13ca13753
lapiscine <beleduedwin@yahoo.fr>
2019-07-04 12:55:11 +0200

    informations film India

---
+++ IndianaJonas.java
@@ -0,0 +1,15 @@
+public  class IndianaJonas {
+
+    public static void main(String[] args) {
+
+        String  indiaJones = "Indiana Jones";
+        boolean ViewYesOrNo = true;
+        double noteOfMovie = 8.2;
+        int realiseYear = 1989;
+        System.out.println("Nom du film : " + indiaJones);
+        System.out.println("Le film est déja vu oui ou non? : " + ViewYesOrNo);
+        System.out.println("L'année de sortie en salle : " + realiseYear);
+
+    }
+
+}
